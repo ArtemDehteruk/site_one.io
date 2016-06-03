d6b17d17d21123a0046feb3e1c69399ab4929df8
Artem Dehteruk <dexteryk@bk.ru>
2016-06-03 20:29:43 +0300

    Updates

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,10 +1,10 @@
 $(function() {
  $('.b-nav__ulist-adaptive-menu').click(function() {
-  if($('.b-nav__ulist-menu').is(':visible')) {
-  $('.b-nav__ulist-menu').removeClass('showitems');
+  if($('.b-nav__ulist, .b-nav__persona').is(':visible')) {
+  $('.b-nav__ulist, .b-nav__persona').removeClass('showitems');
  }
  else {
-  $('.b-nav__ulist-menu').addClass('showitems');
+  $('.b-nav__ulist, .b-nav__persona').addClass('showitems');
  }
 });
 });
